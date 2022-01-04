0052459b94f3238f3d93d250a7ff108255f3db3f
shahbaz <shahbazrock0321@gmail.com>
2022-01-05 01:44:26 +0500

    Add material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home_pg from './pages/Home_pg';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Home_pg />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home_pg.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home_pg = () => {
+    return (
+        <div>
+            home
+        </div>
+    )
+}
+
+export default Home_pg
