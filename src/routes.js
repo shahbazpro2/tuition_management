100366a3cfeb55db047a15606991aeae93fceebf
shahbaz <shahbazrock0321@gmail.com>
2022-01-11 12:44:21 +0500

    Add home route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { url_attendence, url_center, url_course, url_editStudent, url_hq, url_login, url_registerStudent, url_student } from './components/functions/pageUrls'
 import { Login, HqMain, Center, Student, RegisterStudent, Course, Attendence } from './pages'
 export const routes = [
+    { element: <Login />, path: '/' },
     { element: <Login />, path: url_login },
     { element: <HqMain />, path: url_hq },
     { element: <Center />, path: url_center },
