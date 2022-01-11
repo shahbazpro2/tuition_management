e87897735678d21a936726db4828c91328d73741
shahbaz <shahbazrock0321@gmail.com>
2022-01-11 13:05:05 +0500

    Remove unnessary code

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,8 @@ import dotenv from 'dotenv'
 dotenv.config();
 const __dirname = path.resolve();
 const app = express()
-
+app.use(cors())
+app.use(express.json());
 const port = process.env.PORT || 5000
 const buildPath = path.join(__dirname, '..', 'build')
 app.use(express.static(buildPath))
@@ -16,13 +17,6 @@ dbo.connectToServer(function (err) {
         //process.exit();
     }
 }) */
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
