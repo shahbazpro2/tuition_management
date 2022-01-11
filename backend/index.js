3d29f127f0b0ccacbf92e0e8fa7767005e06b8d6
shahbaz <shahbazrock0321@gmail.com>
2022-01-11 12:26:20 +0500

    Config for react with node js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import dotenv from 'dotenv'
 import recordRoutes from './routes/createUser.js'
 import { dbo } from './utils/mongodb.js'
+dotenv.config();
 const app = express()
 
-const port = 5000
+const port = process.env.PORT || 5000
+const buildPath = path.join(__dirname, '..', 'build')
+app.use(express.static(buildPath))
 app.use(recordRoutes);
 dbo.connectToServer(function (err) {
     if (err) {
