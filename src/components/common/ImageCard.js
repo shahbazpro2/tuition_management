6fb39eaac49cea55e7be0c0a8eea182b28017c29
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-01-30 22:24:04 +0500

    Make ImageCard component

---
+++ src/components/common/ImageCard.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+import { Card, CardContent } from '@mui/material'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const ImageCard = ({ title, image, width, link }) => {
+    const navigate = useNavigate()
+    return (
+        <div>
+            <Card className="cursor-pointer" onClick={() => navigate(link)} sx={{ background: '#f9c8ff', height: '130px' }}>
+                <CardContent className='text-center flex items-center justify-center h-full'>
+                    <div>
+                        <div className="flex justify-center">
+                            <img src={`${process.env.PUBLIC_URL}/assets/${image}`} width={width || 55} />
+                        </div>
+                        <div className="text-lg font-medium">
+                            {title}
+                        </div>
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default ImageCard
