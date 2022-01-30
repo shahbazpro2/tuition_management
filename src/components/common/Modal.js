7a2d2682f9b308588d38e3e6ff6051f6930bab64
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-01-31 01:19:20 +0500

    Make modal layout component

---
+++ src/components/common/Modal.js
@@ -0,0 +1,18 @@
+import { Modal } from '@mui/material';
+import React from 'react';
+
+const ModalLayout = (props) => {
+    return <Modal
+        open={props.open}
+        onClose={() => { props.setOpen(false) }}
+    >
+        <div className="w-[95%] md:w-[500px] h-[55%]  overflow-auto absolute rltb-0 m-rltb-auto bg-white rounded-lg p-10" >
+            <div className="text-2xl text-center mb-7">{props.title}</div>
+            <div className="mt-10">
+                {props.children}
+            </div>
+        </div>
+    </Modal>
+};
+
+export default ModalLayout;
