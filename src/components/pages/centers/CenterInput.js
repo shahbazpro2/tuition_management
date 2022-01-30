f1985786353733630b03a4421f258819f0d923bd
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-01-31 01:20:32 +0500

    Add more content

--- src/components/pages/centers/CenterInput.js
+++ src/components/pages/centers/CenterInput.js
@@ -1,10 +1,18 @@
-import { Button } from '@mui/material';
-import React from 'react';
+import { Button, MenuItem } from '@mui/material';
+import React, { useState } from 'react';
+import SelectField from '../../common/textFields/SelectField';
 import TextFieldSimple from '../../common/textFields/TextFieldSimple';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import PICModal from './PICModal';
+import ModalLayout from '../../common/Modal';
+import BankModal from './BankModal';
 
 const CenterInput = () => {
+    const [picModal, setPicModal] = useState(false)
+    const [bankModal, setBankModal] = useState(false)
+
     return <div className="">
-        <form className='space-y-3'>
+        <form className='space-y-4'>
             <TextFieldSimple
                 label="Name"
             />
@@ -14,9 +22,27 @@ const CenterInput = () => {
             <TextFieldSimple
                 label="Business Reg Number"
             />
+            <div className="flex space-x-3 items-center">
+                <SelectField label="PIC Name">
+                </SelectField>
+                <AddCircleIcon sx={{ fontSize: '25px', cursor: 'pointer' }} onClick={() => setPicModal(true)} />
+                <ModalLayout title="PIC Details" open={picModal} setOpen={() => setPicModal(false)}>
+                    <PICModal />
+                </ModalLayout>
+            </div>
+            <div className="flex space-x-3 items-center">
+                <SelectField label="Bank Merchant & Account">
+                </SelectField>
+                <AddCircleIcon sx={{ fontSize: '25px', cursor: 'pointer' }} onClick={() => setBankModal(true)} />
+                <ModalLayout title="Bank Merchant Details" open={bankModal} setOpen={() => setBankModal(false)}>
+                    <BankModal />
+                </ModalLayout>
+            </div>
             <TextFieldSimple
                 label="Center Office Number"
             />
+            <SelectField label="KPI">
+            </SelectField>
             <Button variant="contained" fullWidth>Save</Button>
         </form>
     </div>;
