82318a58d642c3cae09cc494ce8760a123068eaa
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-01-30 22:25:14 +0500

    Replace with ImageCard

--- src/components/pages/hqMain/HqMain.js
+++ src/components/pages/hqMain/HqMain.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import { url_center, url_course } from '../../functions/pageUrls'
-import HqCard from './HqCard'
+import ImageCard from '../../common/ImageCard'
+import { url_attendence, url_center, url_course } from '../../functions/pageUrls'
 
 const HqMain = () => {
     return (
         <div className='mt-10'>
             <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
-                <HqCard title="Center Management" link={url_center} />
-                <HqCard title="Course Management" link={url_course} />
-                <HqCard title="KPI Settings" />
-                <HqCard title="Reports" />
-                <HqCard title="Package Management" />
-                <HqCard title="Inventories Management" />
-                <HqCard title="Billing Center" />
-                <HqCard title="Settings" />
+                <ImageCard title="Centers" image="CentreManagement.png" link={url_center} />
+                <ImageCard title="Courses" image="Course.png" link={url_course} />
+                <ImageCard title="Center's KPI" image="KPI.png" width={60} />
+                <ImageCard title="Packages" image="PackageManagement.png" />
+                <ImageCard title="Inventories" image="Inventories.png" />
+                <ImageCard title="Invoice/Billing" image="BillingReceipt.png" />
+                <ImageCard title="Student" image="StudentsManagement.png" width={70} />
+                <ImageCard title="Attendence" image="Attendance.png" link={url_attendence} />
+                <ImageCard title="Pay HQ" image="Payment.png" />
+                <ImageCard title="Report" image="Report.png" />
+                <ImageCard title="Issue & Request Inventory" image="IssuanceToStudents.png" />
+                <ImageCard title="Received Payment From Student" image="Received.png" />
+                <ImageCard title="Enquires" image="Enquiries.png" />
+                <ImageCard title="Settings" image="Settings.png" />
             </div>
         </div>
     )
