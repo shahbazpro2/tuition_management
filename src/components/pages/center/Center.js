d41d963408f42188c27590f8e06094b5f0ab537d
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-01-30 22:35:07 +0500

    Add table

--- src/components/pages/center/Center.js
+++ src/components/pages/center/Center.js
@@ -1,13 +1,58 @@
 import React from 'react'
 import AddIcon from '@mui/icons-material/Add';
 import Add from '../../common/Add';
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+
+function createData(id, name, location, pic, status) {
+    return { id, name, location, pic, status };
+}
+
+const rows = [
+    createData(1, 'HanoAmpang', 'Ampang', 'Chris', 'Active'),
+    createData(2, 'HanoAmpang', 'Ampang', 'Chris', 'Active'),
+    createData(3, 'HanoAmpang', 'Ampang', 'Chris', 'Inactive'),
+    createData(5, 'HanoAmpang', 'Ampang', 'Chris', 'Active'),
+];
+
 
 const Center = () => {
     return (
         <div className='mt-10'>
             <Add title="Add Center" />
-            <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
-                center
+            <div className="mt-5">
+                <TableContainer component={Paper} sx={{ maxHeight: 440 }}>
+                    <Table stickyHeader sx={{ minWidth: 650 }}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell sx={{ background: '#818181', color: 'white', fontWeight: 'bold' }}>Center ID</TableCell>
+                                <TableCell sx={{ background: '#818181', color: 'white', fontWeight: 'bold' }}>Center Name</TableCell>
+                                <TableCell sx={{ background: '#818181', color: 'white', fontWeight: 'bold' }}>Location</TableCell>
+                                <TableCell sx={{ background: '#818181', color: 'white', fontWeight: 'bold' }}>PIC Name</TableCell>
+                                <TableCell sx={{ background: '#818181', color: 'white', fontWeight: 'bold' }}>Status</TableCell>
+                                <TableCell sx={{ background: '#818181', color: 'white', fontWeight: 'bold' }}></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow
+                                    key={row.name}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell>
+                                        {row.id}
+                                    </TableCell>
+                                    <TableCell align="left"  >{row.name}</TableCell>
+                                    <TableCell align="left"  >{row.location}</TableCell>
+                                    <TableCell align="left"  >{row.pic}</TableCell>
+                                    <TableCell align="left"  >{row.status}</TableCell>
+                                    <TableCell align="left">
+                                        <Button variant="contained" color="success">Edit</Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
         </div>
     )
