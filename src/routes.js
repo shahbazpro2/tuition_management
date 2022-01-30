6d443c4b67b9cbbe7a1883925b8f34d268e7cf4a
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-01-31 01:19:04 +0500

    Add more routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
-import { url_addCenter, url_attendence, url_center, url_course, url_editStudent, url_hq, url_login, url_registerStudent, url_student } from './components/functions/pageUrls'
-import AddCenter from './components/pages/centers/AddCenter'
-import { Login, HqMain, Centers, Student, RegisterStudent, Course, Attendence } from './pages'
+import { url_addCenter, url_attendence, url_centers, url_course, url_editCenter, url_editStudent, url_hq, url_login, url_registerStudent, url_student } from './components/functions/pageUrls'
+import { Login, HqMain, Centers, AddCenter, EditCenter, Student, RegisterStudent, Course, Attendence } from './pages'
 export const routes = [
     { element: <Login />, path: '/' },
     { element: <Login />, path: url_login },
     { element: <HqMain />, path: url_hq },
-    { element: <Centers />, path: url_center },
+    { element: <Centers />, path: url_centers },
     { element: <AddCenter />, path: url_addCenter },
+    { element: <EditCenter />, path: url_editCenter },
     { element: <Student />, path: url_student },
     { element: <RegisterStudent />, path: url_registerStudent },
     { element: <RegisterStudent />, path: url_editStudent },
