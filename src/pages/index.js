a7a50d664c0a8934bc2c65dc55deb6ee95b0ce79
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-01-31 01:22:22 +0500

    Add more components

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Student from './student'
 import RegisterStudent from '../components/pages/student/Register';
 import Course from './course'
 import Attendence from './attendence';
+import AddCenter from '../components/pages/centers/AddCenter';
+import EditCenter from '../components/pages/centers/EditCenter';
 
-
-export { Login, HqMain, Centers, Student, RegisterStudent, Course, Attendence }
+export { Login, HqMain, Centers, AddCenter, EditCenter, Student, RegisterStudent, Course, Attendence }
