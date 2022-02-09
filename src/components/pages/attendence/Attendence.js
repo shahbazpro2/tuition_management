dd96e379380d2086712d2bab825a979daca89820
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-02-10 01:12:21 +0500

    Change attendence content

--- src/components/pages/attendence/Attendence.js
+++ src/components/pages/attendence/Attendence.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import Add from '../../common/Add'
 import TopSecton from './TopSecton'
-import CustomTable from '../../common/CustomTable'
 import AttendenceTable from './AttendenceTable'
+import SelectField from '../../common/textFields/SelectField'
+import TextFieldSimple from '../../common/textFields/TextFieldSimple'
 
 const Attendence = () => {
     return (
-        <div className='mt-7 space-y-10'>
-            <Add title="Create Attendance" />
-            <TopSecton />
+        <div className='content'>
+            <div className="grid grid-cols-6 gap-5">
+                <div className='col-span-1 flex items-center'>Selected Course</div>
+                <div className="col-span-2">
+                    <SelectField label="Course (Language available in course)">
+                    </SelectField>
+                </div>
+                <div className="col-span-3">
+                    <div className="w-[200px] ml-auto">
+                        <TextFieldSimple type="date" label="Date" InputLabelProps={{ shrink: true }} />
+                    </div>
+                </div>
+            </div>
             <div className="mt-7">
                 <AttendenceTable />
             </div>
