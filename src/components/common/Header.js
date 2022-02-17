daf7e127b6c48ee0271b2c693bafe96acdf6380e
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-02-18 01:26:59 +0500

    Add header items

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,20 +1,38 @@
 /* eslint-disable no-undef */
-import { Drawer, List, ListItem, ListItemText } from '@mui/material'
+import { Drawer, List, ListItem, ListItemText, useTheme } from '@mui/material'
 import React, { useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import Container from './Container'
 import MenuIcon from '@mui/icons-material/Menu';
-import { url_contact, url_hq, url_login } from '../functions/pageUrls';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import { url_attendence, url_centerKpi, url_centers, url_contact, url_courses, url_enquires, url_hq, url_inventories, url_issue2Student, url_login, url_packages, url_payByStudent, url_payHq, url_report, url_studentInvoice, url_students } from '../functions/pageUrls';
 import { Toggle } from './Toggle';
 const links = [
     { url: url_login, name: 'TCenter' },
-    { url: url_hq, name: 'HQ' },
-
-
+    { url: url_centers, name: 'Centers' },
+    { url: url_courses, name: 'Courses' },
+    { url: url_centerKpi, name: 'Center KPI' },
+    { url: url_packages, name: 'Packages' },
+    { url: url_inventories, name: 'Inventories' },
+    { url: url_studentInvoice, name: 'Invoice/Billing' },
+    { url: url_students, name: 'Students' },
+    { url: url_attendence, name: 'Attendance' },
+]
+const extraLinks = [
+    { url: url_payHq, name: 'PayHQ' },
+    { url: url_report, name: 'Report' },
+    { url: url_enquires, name: 'Enquiries' },
+    { url: url_issue2Student, name: 'Issue Inventory' },
+    { url: url_payByStudent, name: 'Received Payment' },
+    { url: '/hq', name: 'Setting' },
 ]
 const Header = () => {
     const location = useLocation()
     const [open, setOpen] = useState(false)
+    const navigate = useNavigate()
     return (
         <div className='bg-headcolor dark:bg-gray-900 dark:text-white text-black print:hidden'>
             <Container>
@@ -29,6 +47,20 @@ const Header = () => {
                                 index !== 0 && <Link key={index} to={link.url}>  <div className="cursor-pointer">{link.name}</div></Link>
 
                             ))}
+                            <PopupState variant="popover" popupId="demo-popup-menu">
+                                {(popupState) => (
+                                    <React.Fragment>
+                                        <div className='cursor-pointer' {...bindTrigger(popupState)}>
+                                            More
+                                        </div>
+                                        <Menu {...bindMenu(popupState)}>
+                                            {extraLinks.map(links => (
+                                                <MenuItem key={links.name} onClick={() => { popupState.close(); navigate(links.url) }}>{links.name}</MenuItem>
+                                            ))}
+                                        </Menu>
+                                    </React.Fragment>
+                                )}
+                            </PopupState>
                             <Toggle />
                         </div>
                     </> : <div className="ml-auto space-x-7 hidden md:flex items-center">
@@ -44,7 +76,7 @@ const Header = () => {
                 onClose={() => setOpen(false)}
             >
                 <List>
-                    {links.map((link, index) => (
+                    {[...links, ...extraLinks].map((link, index) => (
                         index !== 0 && <Link key={index} to={link.url}>  <ListItem onClick={() => setOpen(false)} button>
                             <ListItemText primary={link.name} />
                         </ListItem></Link>
