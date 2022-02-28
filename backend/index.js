71d56bec9eafc5f62b4d518fcbe1a2233ec286f3
shahbaz <shahbazrock0321@gmail.com>
2022-02-28 14:14:07 +0500

    Fix cannot get issse

--- backend/index.js
+++ backend/index.js
@@ -16,14 +16,22 @@ dotenv.config();
 const port = process.env.PORT || 8000
 
 //Static path for frontend
-/* const __dirname = path.resolve();
-const buildPath = path.join(__dirname, '..', 'build')
-app.use(express.static(buildPath)) */
 const __dirname = path.resolve();
+/* const buildPath = path.join(__dirname, '..', 'build')
+app.use(express.static(buildPath))
+ */
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build/index.html'), function (err) {
+        if (err) {
+            res.status(500).send(err)
+        }
+    })
+})
+/* const __dirname = path.resolve();
 console.log(path.join(__dirname, '..', 'build', 'index.html'))
 app.get('*', (request, response) => {
     response.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
-});
+}); */
 
 //DB connect
 /* dbo.connectToServer((err) => err && console.error(err)) */
